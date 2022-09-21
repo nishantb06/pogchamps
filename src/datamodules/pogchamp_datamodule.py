8c906fdc4e81b7ad78ae024dbe3a433d7cb982de
Nishant Bhansali <nbhansali06@gmail.com>
2022-09-21 05:58:03 +0000

    pandas import error

--- src/datamodules/pogchamp_datamodule.py
+++ src/datamodules/pogchamp_datamodule.py
@@ -7,7 +7,7 @@ from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
 from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
 
-import pandas
+import pandas as pd
 import numpy as np
 from skimage import io
 import os
